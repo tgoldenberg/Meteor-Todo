ebfa58c3525d7290fb9feced84079a0f479d9648
tgoldenberg <thomasagoldenberg@gmail.com>
2015-08-20 19:34:32 -0400

    add event listener to hide unfinished tasks

--- simple-todos.js
+++ simple-todos.js
@@ -4,12 +4,13 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
 
   Template.body.events({
     "submit .new-task": function(event) {
+      console.log(event);
       event.preventDefault();
       var text = event.target.text.value;
       Tasks.insert({
@@ -17,6 +18,34 @@ if (Meteor.isClient) {
         createdAt: new Date()
       });
       event.target.text.value = "";
+    },
+    "change .hide-completed input": function(event) {
+      Session.set("hideCompleted", event.target.checked);
+    }
+  });
+
+  Template.body.helpers({
+    tasks: function() {
+      if (Session.get("hideCompleted")) {
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function() {
+      return Session.get("hideCompleted");
+    }
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function() {
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+
+    "click .delete": function() {
+      Tasks.remove(this._id);
     }
   });
 }
