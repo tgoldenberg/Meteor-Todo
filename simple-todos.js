d77385bdcd113f731defd1435c688b5cee5a5165
tgoldenberg <thomasagoldenberg@gmail.com>
2015-08-20 18:55:45 -0400

    add behavior to save inserted tasks

--- simple-todos.js
+++ simple-todos.js
@@ -1,10 +1,22 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function(event) {
+      event.preventDefault();
+      var text = event.target.text.value;
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+      event.target.text.value = "";
+    }
   });
 }
